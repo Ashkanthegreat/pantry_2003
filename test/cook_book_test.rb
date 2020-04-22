42fb60b96ea1eaef07a5961eeec5baf32ae3e4e4
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-22 10:41:23 -0600

    Finish Iteration 2

---
+++ test/cook_book_test.rb
@@ -0,0 +1,36 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cook_book'
+
+class CookBookTest < Minitest::Test
+
+  def setup
+    @cookbook = CookBook.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+  end
+
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_cookbook_attributes
+    assert_equal [], @cookbook.recipes
+  end
+
+  def test_cookbook_can_add_recipes
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+end
